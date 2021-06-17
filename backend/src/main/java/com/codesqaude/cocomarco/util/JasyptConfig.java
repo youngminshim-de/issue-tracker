abf2f07d3de8e6730fbdf3d5b6aacbfab0fed1bd
Marco <73640185+95degree@users.noreply.github.com>
2021-06-17 16:42:56 +0900

    Update JasyptConfig.java

--- backend/src/main/java/com/codesqaude/cocomarco/util/JasyptConfig.java
+++ backend/src/main/java/com/codesqaude/cocomarco/util/JasyptConfig.java
@@ -11,7 +11,7 @@ public class JasyptConfig {
     public StringEncryptor stringEncryptor() {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword("cocomarco");
+        config.setPassword("${kye}");
         encryptor.setConfig(config);
         return encryptor;
     }
