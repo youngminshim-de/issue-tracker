9971e037f9207b5a9b3594ab5a302c324d11a06a
Sonjh1306 <yale1306@gmail.com>
2021-07-23 00:00:44 +0900

    [#74] feat: 텍스트필드 부가 기능 구현
    키보드 보일 때 뷰 사이즈 조절, 텍스티필드 버튼 구현

--- iOS/issue-tracker/issue-tracker/ViewController/IssueDetailViewController.swift
+++ iOS/issue-tracker/issue-tracker/ViewController/IssueDetailViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 import Combine
 
-class IssueDetailViewController: UIViewController {
+class IssueDetailViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var issueTitle: UILabel!
     @IBOutlet weak var issueNumber: UILabel!
@@ -9,13 +9,62 @@ class IssueDetailViewController: UIViewController {
     @IBOutlet weak var writeTime: UILabel!
     @IBOutlet weak var writer: UILabel!
     @IBOutlet weak var commentTableView: UITableView!
+    @IBOutlet weak var commentTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCommentTextField()
+        configureCommentTableView()
+        let tapGesture = UITapGestureRecognizer(target: view, action: #selector(view.endEditing))
+        view.addGestureRecognizer(tapGesture)
 
-        commentTableView.register(CommentTableViewCell.nib, forCellReuseIdentifier: CommentTableViewCell.identifier)
+        commentTextField.delegate = self
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    private func configureCommentTextField() {
+        let commentPostButton = UIButton()
+        let image = UIImage(systemName: "arrow.up.circle.fill")
+        commentPostButton.setImage(image, for: .normal)
+        commentPostButton.imageEdgeInsets = .init(top: 0, left: -10, bottom: 0, right: 0)
+        commentTextField.rightView = commentPostButton
+        commentTextField.rightViewMode = .always
+        commentPostButton.addTarget(self, action: #selector(postComment), for: .touchUpInside)
+        
+        commentTextField.layer.masksToBounds = true
+        commentTextField.layer.cornerRadius = 15
+        commentTextField.layer.borderWidth = 1
+        commentTextField.layer.borderColor = UIColor.systemGray3.cgColor
+    }
+    
+    private func configureCommentTableView() {
+        commentTableView.register(CommentTableViewCell.nib, forCellReuseIdentifier: CommentTableViewCell.identifier)
+        configureTableViewFooterView()
+    }
+    
+    private func configureTableViewFooterView() {
+        let footerView = UIView()
+        footerView.backgroundColor = UIColor.clear
+        commentTableView.tableFooterView = footerView
+    }
+    
+    @objc func postComment() {
+        
+    }
+    
+    @objc func keyboardWillShow(_ sender: NSNotification) {
+        self.view.frame.origin.y = -300
+    }
+    
+    @objc func keyboardWillHide(_ sender: NSNotification) {
+        self.view.frame.origin.y = 0
+    }
 }
 
 extension IssueDetailViewController: UITableViewDataSource, UITableViewDelegate {
