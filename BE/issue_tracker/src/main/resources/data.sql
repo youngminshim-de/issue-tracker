edf04590b447147b725d90514cfc0d903d759626
Shion <ehdrhelr@gmail.com>
2021-08-09 21:53:46 +0900

    chore : DB insert data 주석처리

--- BE/issue_tracker/src/main/resources/data.sql
+++ BE/issue_tracker/src/main/resources/data.sql
@@ -5,10 +5,10 @@
 -- values (2, "ehdrhelr@gmail.com", "GITHUB", "프로필 이미지 url", "shion");
 
 -- milestone
-insert into milestone(id, content, created_date, due_date, is_open, title, is_deleted)
-values (1, "백엔드 1주차 마일스톤 내용", "2021-06-01", "2021-06-15", false, "BE 1주차", false),
-       (2, "백엔드 2주차 마일스톤 내용", "2021-06-14", "2021-06-30", true, "BE 2주차", false),
-       (3, "백엔드 3주차 마일스톤 내용", "2021-06-15", "2021-06-30", true, "BE 3주차", false);
+-- insert into milestone(id, content, created_date, due_date, is_open, title, is_deleted)
+-- values (1, "백엔드 1주차 마일스톤 내용", "2021-06-01", "2021-06-15", false, "BE 1주차", false),
+--        (2, "백엔드 2주차 마일스톤 내용", "2021-06-14", "2021-06-30", true, "BE 2주차", false),
+--        (3, "백엔드 3주차 마일스톤 내용", "2021-06-15", "2021-06-30", true, "BE 3주차", false);
 
 -- issue
 -- insert into issue(id, created_time, is_open, title, writer_id, milestone_id, is_deleted)
@@ -19,10 +19,10 @@ values (1, "백엔드 1주차 마일스톤 내용", "2021-06-01", "2021-06-15", 
 --        (5, CURRENT_TIMESTAMP, false, "이슈 제목2", 2, 3, true);
 
 -- label
-insert into label(id, title, content, color, is_deleted)
-values (1, "BE", "백엔드 라벨", "#3DDCFF", false),
-       (2, "FE", "프론트엔드 라벨", "#3DDCFF", true),
-       (3, "iOS", "iOS 라벨", "#3DDCFF", false);
+-- insert into label(id, title, content, color, is_deleted)
+-- values (1, "BE", "백엔드 라벨", "#3DDCFF", false),
+--        (2, "FE", "프론트엔드 라벨", "#3DDCFF", true),
+--        (3, "iOS", "iOS 라벨", "#3DDCFF", false);
 
 -- issue_label
 -- insert into issue_label(issue_id, label_id)
@@ -49,17 +49,17 @@ values (1, "BE", "백엔드 라벨", "#3DDCFF", false),
 
 
 -- emoji
-insert into emoji(id, `name`, is_deleted)
-values (1, ":happy_face", false),
-       (2, ":surprise_face", false),
-       (3, ":heart", false),
-       (4, ":sun", false),
-       (5, ":cloud", false),
-       (6, ":gold_medal", false),
-       (7, ":honux", false),
-       (8, ":tears", false),
-       (9, ":thumb_up", false),
-       (10, ":fire", false);
+-- insert into emoji(id, `name`, is_deleted)
+-- values (1, ":happy_face", false),
+--        (2, ":surprise_face", false),
+--        (3, ":heart", false),
+--        (4, ":sun", false),
+--        (5, ":cloud", false),
+--        (6, ":gold_medal", false),
+--        (7, ":honux", false),
+--        (8, ":tears", false),
+--        (9, ":thumb_up", false),
+--        (10, ":fire", false);
 
 -- -- comment_emoji
 -- insert into comment_emoji(comment_id, emoji_id)
